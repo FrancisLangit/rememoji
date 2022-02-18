14eab9d952131b6868cf1e4e41fa7c9869c26968
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-18 20:46:44 +0800

    Randomize emoji order if and when one of them clicked

--- src/components/Emojis/Emojis.js
+++ src/components/Emojis/Emojis.js
@@ -3,14 +3,15 @@ import { useState, useEffect } from 'react';
 import { Emoji } from '../Emoji/Emoji';
 
 import { getEmojis } from './helpers/getEmojis';
+import { getShuffledArray } from './helpers/getShuffledArray';
 
 const Emojis = () => {
-  const [emojis, setEmojis] = useState([]);
+  const [emojis, setEmojis] = useState(getEmojis(12));
   const [clickedEmojis, setClickedEmojis] = useState([]);
 
   useEffect(() => {
-    setEmojis(getEmojis(12));
-  }, []);
+    setEmojis((emojis) => getShuffledArray([...emojis]));
+  }, [clickedEmojis]);
 
   return (
     <div>
