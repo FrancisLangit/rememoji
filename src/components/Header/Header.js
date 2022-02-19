cb1b874265fe9d9ef7812cbd11189ae8710398bb
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-19 20:01:18 +0800

    Construct header with title and links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,18 @@
+import styles from './Header.module.scss';
+
 const Header = () => {
-  return <div>Rememoji</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Rememoji</div>
+      <div className={styles.links}>
+        <a href="">About</a>
+        <span> • </span>
+        <a href="">How to Play</a>
+        <span> • </span>
+        <a href="">Author</a>
+      </div>
+    </div>
+  );
 };
 
 export { Header };
