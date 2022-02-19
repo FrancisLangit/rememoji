464208baf760e6aec18b88db29a2c17e0f22e49f
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-19 15:24:40 +0800

    Create score tracker and its display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 
-import { Game } from './components/Game/Game';
 import { EndScreen } from './components/EndScreen/EndScreen';
+import { Game } from './components/Game/Game';
 
 const App = () => {
   const [gamestate, setGamestate] = useState(0);
+  const [score, setScore] = useState(-1);
 
   useEffect(() => {
     console.log(`Gamestate: ${gamestate}`);
@@ -15,7 +16,15 @@ const App = () => {
   } else if (gamestate === 1) {
     return <EndScreen text="You win!" setGamestate={setGamestate} />;
   } else {
-    return <Game count={6} gamestate={gamestate} setGamestate={setGamestate} />;
+    return (
+      <Game
+        count={6}
+        score={score}
+        setScore={setScore}
+        gamestate={gamestate}
+        setGamestate={setGamestate}
+      />
+    );
   }
 };
 
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,24 +7,30 @@ import { getShuffledArray } from './helpers/getShuffledArray';
 import { hasDuplicates } from './helpers/hasDuplicates';
 import styles from './Game.module.scss';
 
-const Game = ({ count, gamestate, setGamestate }) => {
+const Game = ({ count, score, setScore, gamestate, setGamestate }) => {
   const [emojis, setEmojis] = useState(getEmojis(count));
   const [clickedEmojis, setClickedEmojis] = useState([]);
 
-  // Shuffle emojis state array whenever a user clicks on an emoji.
+  // If the user clicks on an emoji previously clicked on, the game is lost.
+  // Else, increment score and shuffle gameboard when a user clicks on an emoji.
   useEffect(() => {
     console.log(`Clicked: ${clickedEmojis}`);
-    setEmojis((emojis) => getShuffledArray([...emojis]));
-    hasDuplicates(clickedEmojis) && setGamestate(-1);
-  }, [clickedEmojis, setGamestate]);
+    if (hasDuplicates(clickedEmojis)) {
+      setGamestate(-1);
+    } else {
+      setEmojis((emojis) => getShuffledArray([...emojis]));
+      setScore((score) => score + 1);
+    }
+  }, [clickedEmojis, setGamestate, setScore]);
 
-  // Restart the game if and when passed prop gamestate is -2.
+  // Restart the game if and when passed prop "gamestate" is -2.
   useEffect(() => {
     if (gamestate === -2) {
       setEmojis(getEmojis(count));
       setGamestate(0);
+      setScore(0);
     }
-  }, [count, gamestate, setGamestate]);
+  }, [count, gamestate, setGamestate, setScore]);
 
   // Detect if the game has been won.
   useEffect(() => {
@@ -34,15 +40,18 @@ const Game = ({ count, gamestate, setGamestate }) => {
   }, [clickedEmojis, count, setGamestate]);
 
   return (
-    <div className={styles.container}>
-      {emojis.map((emoji) => (
-        <Emoji
-          key={`game-${emoji}`}
-          emoji={emoji}
-          clickedEmojis={clickedEmojis}
-          setClickedEmojis={setClickedEmojis}
-        />
-      ))}
+    <div>
+      <div className={styles.score}>Score: {score}</div>
+      <div className={styles.container}>
+        {emojis.map((emoji) => (
+          <Emoji
+            key={`game-${emoji}`}
+            emoji={emoji}
+            clickedEmojis={clickedEmojis}
+            setClickedEmojis={setClickedEmojis}
+          />
+        ))}
+      </div>
     </div>
   );
 };
