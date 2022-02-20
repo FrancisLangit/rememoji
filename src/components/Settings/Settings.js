d31d38389f8ce46965390405a9310da135fca7b6
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-20 14:16:32 +0800

    Refactor Settings component

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,47 +1,22 @@
 import { useState } from 'react';
 
-import styles from './Settings.module.scss';
-
-const Button = ({ setIsShow }) => {
-  return (
-    <div className={styles.buttonContainer}>
-      <div className={styles.button} onClick={() => setIsShow(true)}>
-        ⚙️ Settings
-      </div>
-    </div>
-  );
-};
+import { Button } from './components/Button/Button';
+import { Form } from './components/Form/Form';
 
 const Settings = ({ emojiCount, setEmojiCount, setGamestate }) => {
-  const [value, setValue] = useState(emojiCount);
   const [isShow, setIsShow] = useState(false);
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setEmojiCount(parseInt(value));
-    setGamestate(-2);
-  };
-
   return (
-    <div>
+    <>
       {isShow ? (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="number"
-            value={value}
-            min={2}
-            max={30}
-            onChange={handleChange}
-          />
-          <input type="submit" value="Submit" />
-        </form>
+        <Form
+          emojiCount={emojiCount}
+          setEmojiCount={setEmojiCount}
+          setGamestate={setGamestate}
+        />
       ) : null}
       <Button setIsShow={setIsShow} />
-    </div>
+    </>
   );
 };
 
