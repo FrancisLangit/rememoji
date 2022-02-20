7a2bfae1311b4021d2964982b7b8d401489be843
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-20 14:34:16 +0800

    Style settings form as modal

--- src/components/Settings/components/Form/Form.js
+++ src/components/Settings/components/Form/Form.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import styles from './Form.module.scss';
+
 const Form = ({ emojiCount, setEmojiCount, setGamestate }) => {
   const [value, setValue] = useState(emojiCount);
 
@@ -14,16 +16,20 @@ const Form = ({ emojiCount, setEmojiCount, setGamestate }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="number"
-        value={value}
-        min={2}
-        max={30}
-        onChange={handleChange}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            value={value}
+            min={2}
+            max={30}
+            onChange={handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
   );
 };
 
